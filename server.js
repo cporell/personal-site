fdbdb42f3099a4fe950334d17af3e464faca1d6f
cporell <cgporell@wpi.edu>
2017-06-25 22:19:46 -0400

    Add router

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 var express = require('express');
+var router = require('./public/routing/router');
 
 var app = express();
 var port = process.env.PORT || 3000;
 
-app.get('/', function(req, res){
-	res.send('Hello world!');
-});
+app.use('/', router);
 
 app.listen(port, function() {
 	console.log('Node app is running at localhost:' + port);
