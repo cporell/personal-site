c56bf038b6be95165adfd3f6c30f4b7dc85d915b
cporell <cgporell@wpi.edu>
2017-06-25 22:39:02 -0400

    Iterative commit

--- public/routing/router.js
+++ public/routing/router.js
@@ -5,7 +5,7 @@ var router = express.Router();
 
 // load the main page on reaching the base dir
 router.get('/', function(req, res){
-	res.sendFile(path.join(__dirname + '../template.html');
+	res.sendFile(path.join(__dirname + '/../template.html'));
 });
 
 module.exports = router;
