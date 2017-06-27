f552eebe0f5b489087f8355435eb1075e5e37cab
cporell <cgporell@wpi.edu>
2017-06-27 07:22:36 -0400

    Finally serving static

--- server.js
+++ server.js
@@ -12,5 +12,4 @@ app.listen(port, function() {
 	console.log('Node app is running at localhost:' + port);
 });
 
-// app.use(express.static(path.join(__dirname, '/public')));
-app.use('/public', express.static(__dirname + '/public'));
+app.use('/app/public', express.static(__dirname + '/app/public'));
