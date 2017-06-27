6282c8fa2440877b0ced24f7ed51c9cb92c08e03
cporell <cgporell@wpi.edu>
2017-06-27 07:18:24 -0400

    Iterative commit

--- server.js
+++ server.js
@@ -12,4 +12,5 @@ app.listen(port, function() {
 	console.log('Node app is running at localhost:' + port);
 });
 
-app.use(express.static(path.join(__dirname, '/public')));
+// app.use(express.static(path.join(__dirname, '/public')));
+app.use('/public', express.static(__dirname + '/public'));
