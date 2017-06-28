adb8d308017b2578f2747eb5e130bb0f5a00a73a
cporell <cgporell@wpi.edu>
2017-06-27 23:38:51 -0400

    Major CSS work

--- app/routes/router.js
+++ app/routes/router.js
@@ -14,6 +14,22 @@ router.get('/whats-new', function(req, res){
 	whatsNewController.loadWhatsNewHome(req, res);
 });
 
+router.get('/about', function(req, res){
+	res.sendFile(path.join(__dirname + '/../pages/about.html'));
+});
+
+router.get('/games', function(req, res){
+	res.sendFile(path.join(__dirname + '/../pages/games.html'));
+});
+
+router.get('/projects', function(req, res){
+	res.sendFile(path.join(__dirname + '/../pages/projects.html'));
+});
+
+router.get('/contact', function(req, res){
+	res.sendFile(path.join(__dirname + '/../pages/contact.html'));
+});
+
 /*
 router.get('*', function(req, res){
 	res.sendFile(path.join(__dirname + '/../404page.html'));
