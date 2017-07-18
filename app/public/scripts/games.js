f867b4260d895e9fa1896c22ffd0441f090b64ba
cporell <cgporell@wpi.edu>
2017-07-18 00:22:31 -0400

    Games data and layout
    Add games data for main games page
    Set layout (still needs tweaking)
    Add some project data for later

--- app/public/scripts/games.js
+++ app/public/scripts/games.js
@@ -7,43 +7,40 @@
  // title, image, desc
 function buildGamesTemplate(elt){
 	var templateStr = '';
-	templateStr += '<div class="gameBlock">';
+	templateStr += '<span class="gameBlock">';
 	templateStr += '<a href="/games/' + elt.title + '">';
-	templateStr += '<img class="gamePic" src="/app/public/images/' + elt.img + '.png"><br/>';
-	templateStr += '<p>' + elt.title + '<br/>';
+	templateStr += '<img class="gamePic" src="/app/public/images/gameCovers/' + elt.img + '.png"><br/>';
+	templateStr += '<p><span class="title">' + elt.title + '</span><br/>';
 	templateStr += elt.desc + '</p>';
 	templateStr += '</a>';
-	templateStr += '</div>';
+	templateStr += '</span>';
 	return templateStr;
 }
 
 function load(){
 
-document.getElementById('feature').innerHTML += 'Hello world <br/>';
+//document.getElementById('feature').innerHTML += 'Hello world <br/>';
 
 var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function(){
 	if(this.readyState == 4 && this.status == 200){
 		myarr = JSON.parse(this.responseText);
 		var counter = 0;
+		document.getElementById('feature').innerHTML += '<div class="gameRow">';
 		myarr.forEach(function(elt){
 			document.getElementById('feature').innerHTML += buildGamesTemplate(elt);
-			counter++;
 			if(counter >= 2){
 				counter = 0;
-				document.getElementById('feature').innerHTML += '<br/>';
+				document.getElementById('feature').innerHTML += '</div>';
+				document.getElementById('feature').innerHTML += '<div class="gameRow">';
 			}
-			/*
-			document.getElementById('feature').innerHTML += '<img class=bioPic src="/app/public/images/' + elt.img + '.jpg"><br/>';
-			document.getElementById('feature').innerHTML += elt.title + '<br/>';
-			document.getElementById('feature').innerHTML += elt.desc + '<br/>';
-			document.getElementById('feature').innerHTML += '<br/>';
-			*/
-			
+			else{
+				counter++;
+			}
 		});
 	}
 };
-xmlhttp.open("GET", "/app/public/pageContent/test.txt", true);
+xmlhttp.open("GET", "/app/public/pageContent/games.txt", true);
 xmlhttp.send();
 	
 }
