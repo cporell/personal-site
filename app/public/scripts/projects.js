3c4e22e6dcdb25a922d122806dd2cc0f3e432e89
Porell, Connor G <cporell@mitre.org>
2017-09-12 18:06:24 -0400

    slideshow work

--- app/public/scripts/projects.js
+++ app/public/scripts/projects.js
@@ -18,29 +18,56 @@ function buildProjectTemplate(elt){
 }
 
 function load(){
-
-var xmlhttp = new XMLHttpRequest();
-xmlhttp.onreadystatechange = function(){
-	if(this.readyState == 4 && this.status == 200){
-		myarr = JSON.parse(this.responseText);
-		var counter = 0;
-		document.getElementById('feature').innerHTML += '<div class="projectRow">';
-		myarr.forEach(function(elt){
-			document.getElementById('feature').innerHTML += buildProjectTemplate(elt);
-			if(counter >= 1){
-				counter = 0;
-				document.getElementById('feature').innerHTML += '</div>';
-				document.getElementById('feature').innerHTML += '<div class="projectRow">';
-			}
-			else{
-				counter++;
-			}
-		});
-	}
-};
-xmlhttp.open("GET", "/app/public/pageContent/projects.txt", true);
-xmlhttp.send();
-	
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.onreadystatechange = function(){
+		if(this.readyState == 4 && this.status == 200){
+			myarr = JSON.parse(this.responseText);
+			var counter = 0;
+			document.getElementById('feature').innerHTML += '<div class="projectRow">';
+			myarr.forEach(function(elt){
+				document.getElementById('feature').innerHTML += buildProjectTemplate(elt);
+				if(counter >= 1){
+					counter = 0;
+					document.getElementById('feature').innerHTML += '</div>';
+					document.getElementById('feature').innerHTML += '<div class="projectRow">';
+				}
+				else{
+					counter++;
+				}
+			});
+		}
+	};
+	xmlhttp.open("GET", "/app/public/pageContent/projects.txt", true);
+	xmlhttp.send();
 }
 
-load();
+//load();
+
+/**
+ * Builds the slideshow by reading in the projects from the txt file
+ * and then creating a slide for each one.
+ */
+function buildSlideshow(){
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.onreadystatechange = function(){
+		if(this.readyState == 4 && this.status == 200){
+			myarr = JSON.parse(this.responseText);
+			var counter = 0;
+			myarr.forEach(function(elt){
+				counter++;
+				var slideshowHTMLstring = '<div class="slide fade">';
+				slideshowHTMLstring += '<div class="slideshow-number-text">' + counter + '/' + myarr.length + '</div>';
+				slideshowHTMLstring += '<img src="/app/public/images/projectCovers/' + elt.img + '.png" style="width:100%">';
+				slideshowHTMLstring += '<div class="slideshow-caption-text">' + elt.desc + '</div>';
+				slideshowHTMLstring += '</div>';
+				document.getElementById('slideshow-container').innerHTML += slideshowHTMLstring;
+				var dotsHTMLstring = '<span class="dot" onclick="currentSlide('+counter+')"></span>';
+				document.getElementById('dots').innerHTML += dotsHTMLstring;
+			});
+		}
+	};
+	xmlhttp.open("GET", "/app/public/pageContent/projects.txt", true);
+	xmlhttp.send();
+}
+
+buildSlideshow();
--- app/routes/router.js
+++ app/routes/router.js
@@ -45,28 +45,6 @@ router.get('*', function(req, res){
 router.get('/whats-new', function(req, res){
 	whatsNewController.loadWhatsNewHome(req, res);
 });
-
-router.get('/about', function(req, res){
-	//aboutController.loadAbout(req, res);
-	res.sendFile(path.join(__dirname + '/../pages/about.html'));
-});
-
-router.get('/games', function(req, res){
-	res.sendFile(path.join(__dirname + '/../pages/games.html'));
-});
-
-router.get('/projects', function(req, res){
-	res.sendFile(path.join(__dirname + '/../pages/projects.html'));
-});
-
-router.get('/contact', function(req, res){
-	res.sendFile(path.join(__dirname + '/../pages/contact.html'));
-});
-
-
-router.get('*', function(req, res){
-	res.sendFile(path.join(__dirname + '/../pages/404page.html'));
-});
 */
 
 module.exports = router;
