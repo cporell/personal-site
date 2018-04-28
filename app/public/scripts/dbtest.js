819a9ad2105e32bf711b3b803a4700c62a7de0ae
Porell, Connor G <CPORELL@MITRE.ORG>
2018-04-27 20:12:12 -0400

    more DB stuff

--- app/public/scripts/dbtest.js
+++ app/public/scripts/dbtest.js
@@ -11,6 +11,35 @@ var loki = require('lokijs');
 // console.log(children.find({'name' : 'Sleipnir'}));
 // console.log(children.find({legs: { '$gt':2}}));
 
-var pagesDB = new loki('/app/public/pageContent/pageContent.db');
-var pageContent = pagesDB.getCollection('pageContent');
-console.log(pageContent.get(0));
+var pagesDB = new loki('pageContent.db');
+pagesDB.loadDatabase({}, load);
+
+function load(){
+    var pageContents = pagesDB.getCollection('pageContent') || pagesDB.addCollection('pageContent');
+    if(pageContents.data.length === 0){
+        pageContents.insert({
+            "type": "subsites",
+            "img": "whats-new",
+            "title": "What's New in Wizard101",
+            "desc": "A catch-up for new/returning Wizard101 players, created for the /r/wizard101 community."
+        });
+        pageContents.insert({
+            "type": "projects",
+            "img": "infiltration",
+            "title": "Community Infiltration",
+            "desc": "An investigative infiltration into an online community to research conflicts in said community."
+        });
+        pageContents.insert({
+            "type": "projects",
+            "img": "good_bad_difficulty",
+            "title": "Good and Bad Difficulty in Games",
+            "desc": "A talk given to WPI's Game Developers Association on the difference between good and bad difficulty in games."
+        });
+    }
+
+    console.log(pageContents.get(1));
+    console.log(pageContents.find({'img': 'good_bad_difficulty'}));
+    console.log(pageContents.get(3));
+}
+//var pageContent = pagesDB.getCollection('pageContent');
+//console.log(pageContent.get(0));
--- app/public/scripts/lib/bundle.js
+++ app/public/scripts/lib/bundle.js
@@ -3,22 +3,49 @@
 },{}],2:[function(require,module,exports){
 var loki = require('lokijs');
 
-var db = new loki('loki.json');
-var children = db.addCollection('children');
+// var db = new loki('loki.json');
+// var children = db.addCollection('children');
 
-children.insert({name: 'Sleipnir', legs:8});
-children.insert({name: 'Jormungandr', legs:0});
-children.insert({name: 'Hel', legs: 2});
+// children.insert({name: 'Sleipnir', legs:8});
+// children.insert({name: 'Jormungandr', legs:0});
+// children.insert({name: 'Hel', legs: 2});
 
-console.log(children.get(1));
-console.log(children.find({'name' : 'Sleipnir'}));
-console.log(children.find({legs: { '$gt':2}}));
+// console.log(children.get(1));
+// console.log(children.find({'name' : 'Sleipnir'}));
+// console.log(children.find({legs: { '$gt':2}}));
 
-/////////////////////
+var pagesDB = new loki('pageContent.db');
+pagesDB.loadDatabase({}, load);
 
-var pagesDB = new loki('/app/public/pageContent/pageContent.db');
-var pageContent = pagesDB.getCollection('pageContent');
-console.log(pageContent.get(0));
+function load(){
+    var pageContents = pagesDB.getCollection('pageContent') || pagesDB.addCollection('pageContent');
+    if(pageContents.data.length === 0){
+        pageContents.insert({
+            "type": "subsites",
+            "img": "whats-new",
+            "title": "What's New in Wizard101",
+            "desc": "A catch-up for new/returning Wizard101 players, created for the /r/wizard101 community."
+        });
+        pageContents.insert({
+            "type": "projects",
+            "img": "infiltration",
+            "title": "Community Infiltration",
+            "desc": "An investigative infiltration into an online community to research conflicts in said community."
+        });
+        pageContents.insert({
+            "type": "projects",
+            "img": "good_bad_difficulty",
+            "title": "Good and Bad Difficulty in Games",
+            "desc": "A talk given to WPI's Game Developers Association on the difference between good and bad difficulty in games."
+        });
+    }
+
+    console.log(pageContents.get(1));
+    console.log(pageContents.find({'img': 'good_bad_difficulty'}));
+    console.log(pageContents.get(3));
+}
+//var pageContent = pagesDB.getCollection('pageContent');
+//console.log(pageContent.get(0));
 },{"lokijs":4}],3:[function(require,module,exports){
 /*
   Loki IndexedDb Adapter (need to include this script to use it)
